62e82ec1ecf49db5874cb9725d9915c58254549a
Promigo Ventures <promigoventures@gmail.com>
2024-11-11 20:24:40 +0100

    Added a python file

---
+++ Assignment4.py
@@ -0,0 +1,7 @@
+first_number = int(input("Enter your first number: "))
+second_number = int(input("Enter your second number: "))
+
+print("Addition: ", first_number + second_number)
+print("Subtraction: ", first_number - second_number)
+print("Multiplication: ", first_number * second_number)
+print("Division: ", first_number / second_number)
